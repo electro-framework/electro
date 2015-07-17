41e5908363f0319d652092779a5405a62e66c82f
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-17 20:02:01 +0100

    Moved composer.json to the root folder. Make it so that all package management and task running can be done from the root folder.

--- private/config/env.php
+++ private/config/env.php
@@ -1,6 +1,6 @@
 <?php
 use Dotenv\Dotenv;
 
-$dotenv = new Dotenv(dirname (__DIR__));
+$dotenv = new Dotenv(dirname(dirname (__DIR__)));
 $dotenv->load();
 $dotenv->required (['DB_DRIVER', 'DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD']);
