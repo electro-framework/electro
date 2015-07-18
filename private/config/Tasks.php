f00053474271290c4dbd9500aff0165ca3e0f6b5
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-19 00:35:56 +0100

    Added folder structure config paths to the app's config. New RoboFile.

---
+++ private/config/Tasks.php
@@ -0,0 +1,11 @@
+<?php
+use Selene\PresetTasks;
+
+/**
+ * The project's console commands configuration for Selene's task runner.
+ *
+ * @see http://robo.li
+ */
+class Tasks extends PresetTasks
+{
+}
--- private/config/application.ini.php
+++ private/config/application.ini.php
@@ -8,17 +8,27 @@ return [
     'appName'             => 'Your App',
     'title'               => '@ - Your App',
     'defaultURI'          => '/',
-    'autoControllerClass' => 'Selene\\Controller',
     #'favicon'             => '',
+    'autoControllerClass' => 'Selene\Controller',
+    'tasksClass'          => 'Tasks',
     'translation'         => false,
     #'languages'           => [],
     'globalSessions'      => false,
     'isSessionRequired'   => false,
 
-    'modules' => [
+    'storagePath'         => 'private/storage',
+    'imageArchivePath'    => 'private/storage/images',
+    'fileArchivePath'     => 'private/storage/files',
+    'cachePath'           => 'private/storage/cache',
+    'imagesCachePath'     => 'private/storage/cache/images',
+    'modulesPath'         => 'private/modules',
+    'defaultModulesPath'  => 'private/packages',
+    'configPath'          => 'private/config',
+
+    'modules'             => [
       'selene-framework/welcome',
       'selene-framework/application-builder',
     ],
 
-  ]
+  ],
 ];
