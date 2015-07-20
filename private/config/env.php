d1b0d19b3b04149d32345beb6d0540b0b7a7ce84
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-20 03:06:58 +0100

    Changed the application's bootstrap.

--- private/config/env.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Dotenv\Dotenv;
-
-$dotenv = new Dotenv(dirname(dirname (__DIR__)));
-$dotenv->load();
-$dotenv->required (['DB_DRIVER', 'DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD']);
