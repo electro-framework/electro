364f804df238ca77bff312841d09590742ded2f6
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-21 01:43:25 +0100

    Class rename.

--- private/config/Tasks.php
+++ private/config/Tasks.php
@@ -1,12 +1,12 @@
 <?php
-use Selene\FrameworkTasks;
+use Selene\CoreCommands;
 
 /**
  * Your project's console commands for Selene's task runner.
  *
  * @see http://robo.li
  */
-class Tasks extends FrameworkTasks
+class Tasks extends CoreCommands
 {
   // Add your commands here.
 }
