f0f16dfb995b8604d12fa8a67fc5011fb892d06a
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-29 23:29:46 +0100

    FIX: broken stuff.

--- private/modules/selene-framework/application-builder/src/Controllers/Index.php
+++ private/modules/selene-framework/application-builder/src/Controllers/Index.php
@@ -1,33 +1,40 @@
 <?php
 namespace Selene\ApplicaitonBuilder\Controllers;
-use Packagist\Api\Client;
 use Selene\Controller;
+use Selene\Lib\PackagistAPI;
 
 class Index extends Controller
 {
   const ref = __CLASS__;
 
-  protected function viewModel ()
-  {
-    $client = new Client();
-
-    return [
-      'packages' => $client->all (['vendor' => 'selene-framework']),
-    ];
-  }
-
   protected function render ()
   { ?>
     <Main>
 
-      <h3>Available modules</h3>
+      <h3>Available plugin modules</h3>
 
-      <Repeat for="{{ !packages }}">
-        <li>{{ #self }}
-      </Repeat>
+      <div style="display:inline-block">
+        <Repeat for="{{ !packages }}">
+          <p>
+            <label for="P{{ #ord }}">
+              <input id="P{{ #ord }}" type="checkbox">
+              {{ name }}
+            </label>
+        </Repeat>
+
+        <p align="right">
+          <button class="btn">Install</button>
+      </div>
 
     </Main>
 
     <?php
+  }
+
+  protected function viewModel ()
+  {
+    return [
+      'packages' => (new PackagistAPI)->type ('selene-plugin')->search (true),
+    ];
   }
 }
--- private/modules/selene-framework/welcome/src/Controllers/Index.php
+++ private/modules/selene-framework/welcome/src/Controllers/Index.php
@@ -22,19 +22,12 @@ class Index extends Controller
   { ?>
     <Master>
 
-      <div class="banner">
-        <div class="title">Selene
-          <div class="quote">rapid web development</div>
-        </div>
-        <div class="by">made by <a href="http://impactwave.com" target="_blank">Impactwave</a></div>
-      </div>
-
       <div class="intro">
 
         <h1>Well done!</h1>
         <p>You have succesfully installed Selene on your computer.</p><br><br>
 
-        <h2>Now what?</h2>
+        <h2>Next steps</h2>
         <div class="text">
           <p>The <i>express way</i> to start is by opening the Modules Manager and installing some pre-made moules on
              this application.</p>
@@ -53,6 +46,8 @@ class Index extends Controller
 
       </div>
 
+      <div class="quote">rapid web development</div>
+      <div class="by">made by <a href="http://impactwave.com" target="_blank">Impactwave</a></div>
     </Master>
 
     <?php
