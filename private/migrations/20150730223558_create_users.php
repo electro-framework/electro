f7b2a7a4e723fc57c1a9c9159af0ead60e65fd35
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-31 01:55:48 +0100

    Began Phinx integration.

---
+++ private/migrations/20150730223558_create_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+  /**
+   * Change Method.
+   *
+   * Write your reversible migrations using this method.
+   *
+   * More information on writing migrations is available here:
+   * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+   */
+  public function change ()
+  {
+    $this
+      ->table ('users')
+      ->addColumn ('username', 'string', ['limit' => 30])
+      ->addIndex (['username'], ['unique' => true])
+      ->addColumn ('password', 'string', ['limit' => 60])
+      ->addColumn ('token', 'string', ['limit' => 60])
+      ->addColumn ('registrationDate', 'datetime')
+      ->addColumn ('lastLogin', 'datetime')
+      ->addColumn ('role', 'string', ['limit' => 10])
+      ->addColumn ('active', 'boolean', ['default' => 0])
+      ->create ();
+    $now = date ('Y-m-d H:i:s');
+    $this->query ("
+      INSERT INTO users (username, registrationDate, role, active)
+      VALUES ('admin', '$now', 'developer', 1);
+");
+  }
+}
