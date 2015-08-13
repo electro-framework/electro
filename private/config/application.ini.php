55e198061624d52a7bb3b7a6a18fc85558575a54
Cláudio Silva <claudio.silva@impactwave.com>
2015-08-14 00:31:41 +0100

    Improved application.ini

--- private/config/application.ini.php
+++ private/config/application.ini.php
@@ -4,27 +4,30 @@
 return [
   'main' => [
 
-    'name'                => 'site',
+    'name'                => 'site',          // session cookie name
     'appName'             => 'Your App',
-    'title'               => '@ - Your App',
+    'title'               => '@ - Your App',  // @ = page title
     'defaultURI'          => '/',
-    #'favicon'             => '',
+    #'favicon'             => '',             // uncomment only if required
     'autoControllerClass' => 'Selene\Controller',
-    'tasksClass'          => 'Tasks',
-    'translation'         => false,
-    'languages'         => [
-      'pt:pt-PT:Português:pt_PT|pt_PT.UTF-8|ptg'
-    ],
-    'globalSessions'      => false,
-    'isSessionRequired'   => false,
+    'subApplications'     => [],
+    'URINotFoundURL'      => false,           // custom 404 page
 
-    'modules'             => [
-      'selene-framework/core-tasks',
-      'selene-framework/welcome',
-      'selene-framework/application-builder',
-      'selene-framework/matisse-components',
-      'selene-framework/migrations'
+    // Localization:
+    'translation'         => false,           // enable translations; default language is selected on .env
+    'languages'           => [
+      'en:en-US:English:en_US|en_US.UTF-8|us',
+      'pt:pt-PT:Português:pt_PT|pt_PT.UTF-8|ptg',
     ],
 
+    // Sessions:
+    'globalSessions'      => false,     // share the sessio between the application and its sub-applications?
+    'requireLogin'        => false,     // require login for this application?
+
+    // For use with the admin-module:
+    'homeIcon'            => '',
+    'homeTitle'           => 'Home',
+    'userModel'           => '',
+    'loginView'           => '',
   ],
 ];
