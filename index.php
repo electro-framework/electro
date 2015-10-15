d7837d4f7a302d2edbd4300b95b7af8988b1454a
Cláudio Silva <claudio.silva@impactwave.com>
2015-10-15 15:11:21 +0100

    Require the framework v1. Support subsystems.

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-//Uncomment the following line to log errors to the current directory, for troubleshooting.
+/*
+ * Temporarily uncomment the following line for troubleshooting on restricted hosting environments.
+ * It enables error logging to a file the project's root directory.
+*/
 //ini_set ('error_log', __DIR__ . '/error.log');
 
+// Start the class autoloader.
 require "private/packages/autoload.php";
 
+/*
+ * Run the framework, which then runs the web application on the current directory.
+ */
 $application = new Selenia\Application();
-
 $application->run (__DIR__);
