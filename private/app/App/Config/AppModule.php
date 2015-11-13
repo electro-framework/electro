3f41088a154c4038266ee1f7ec1eb0cd649d8a2c
Cláudio Silva <claudio.silva@impactwave.com>
2015-11-13 16:20:26 +0000

    Use class references on the middleware stack

--- private/app/App/Config/AppModule.php
+++ private/app/App/Config/AppModule.php
@@ -2,28 +2,36 @@
 namespace App\Config;
 
 use Selenia\Application;
-use Selenia\Core\Assembly\Services\ModuleServices;
+use Selenia\Authentication\Middleware\AuthenticationMiddleware;
+use Selenia\Debugging\Middleware\WebConsoleMiddleware;
+use Selenia\ErrorHandling\Middleware\ErrorHandlingMiddleware;
+use Selenia\FileServer\Middleware\FileServerMiddleware;
+use Selenia\Http\Middleware\CompressionMiddleware;
+use Selenia\Http\Middleware\CsrfMiddleware;
+use Selenia\HttpMiddleware\Middleware\URINotFoundMiddleware;
 use Selenia\Interfaces\MiddlewareStackInterface;
 use Selenia\Interfaces\ModuleInterface;
+use Selenia\Localization\Middleware\LanguageMiddleware;
+use Selenia\Localization\Middleware\TranslationMiddleware;
+use Selenia\Routing\Middleware\RoutingMiddleware;
+use Selenia\Sessions\Middleware\SessionMiddleware;
 
 class AppModule implements ModuleInterface
 {
   function boot (Application $app = null, MiddlewareStackInterface $middleware = null)
   {
     $middleware
-      ->addIf (!$app->debugMode, 'Selenia\Http\Middleware\CompressionMiddleware')
-      ->addIf ($app->debugMode, 'Selenia\Debugging\Middleware\WebConsoleMiddleware')
-      ->add ('Selenia\ErrorHandling\Middleware\ErrorHandlingMiddleware')
-      ->add ('Selenia\Sessions\Middleware\SessionMiddleware')
-      ->add ('Selenia\Http\Middleware\CsrfMiddleware')
-      ->add ('Selenia\Localization\Middleware\LanguageMiddleware')
-      ->add ('Selenia\Authentication\Middleware\AuthenticationMiddleware')
-      ->add ('Selenia\FileServer\Middleware\FileServerMiddleware')
-      ->add ('Selenia\Localization\Middleware\TranslationMiddleware')
-      ->add ('Selenia\Routing\Middleware\RoutingMiddleware')
-      ->add ('Selenia\HttpMiddleware\Middleware\URINotFoundMiddleware');
+      ->addIf (!$app->debugMode, CompressionMiddleware::class)
+      ->addIf ($app->debugMode, WebConsoleMiddleware::class)
+      ->add (ErrorHandlingMiddleware::class)
+      ->add (SessionMiddleware::class)
+      ->add (CsrfMiddleware::class)
+      ->add (LanguageMiddleware::class)
+      ->add (AuthenticationMiddleware::class)
+      ->add (FileServerMiddleware::class)
+      ->add (TranslationMiddleware::class)
+      ->add (RoutingMiddleware::class)
+      ->add (URINotFoundMiddleware::class);
   }
-
-  function configure (ModuleServices $module) { }
 
 }
