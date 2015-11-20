8d28bbdedb558e2a7a69e9bed8d776f08d4e263c
Cláudio Silva <claudio.silva@impactwave.com>
2015-11-20 17:59:41 +0000

    Removed auth middleware from main middlware pipeline

--- private/app/App/Config/AppModule.php
+++ private/app/App/Config/AppModule.php
@@ -2,7 +2,6 @@
 namespace App\Config;
 
 use Selenia\Application;
-use Selenia\Authentication\Middleware\AuthenticationMiddleware;
 use Selenia\Debugging\Middleware\WebConsoleMiddleware;
 use Selenia\ErrorHandling\Middleware\ErrorHandlingMiddleware;
 use Selenia\FileServer\Middleware\FileServerMiddleware;
@@ -28,7 +27,6 @@ class AppModule implements ModuleInterface
         SessionMiddleware::class,
         CsrfMiddleware::class,
         LanguageMiddleware::class,
-        AuthenticationMiddleware::class,
         FileServerMiddleware::class,
         TranslationMiddleware::class,
         RoutingMiddleware::class,
