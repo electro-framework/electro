81889d9872b1a284f6550d0820e6c6d35672a796
Cláudio Silva <claudio.silva@impactwave.com>
2016-01-07 18:43:24 +0000

    Auto Routing support.

--- private/app/App/Config/AppModule.php
+++ private/app/App/Config/AppModule.php
@@ -13,6 +13,7 @@ use Selenia\Interfaces\Http\Shared\ApplicationRouterInterface;
 use Selenia\Interfaces\ModuleInterface;
 use Selenia\Localization\Middleware\LanguageMiddleware;
 use Selenia\Localization\Middleware\TranslationMiddleware;
+use Selenia\Routing\Middleware\AutoRoutingMiddleware;
 use Selenia\Sessions\Middleware\SessionMiddleware;
 
 class AppModule implements ModuleInterface
@@ -30,6 +31,7 @@ class AppModule implements ModuleInterface
           CsrfMiddleware::class,
           LanguageMiddleware::class,
           TranslationMiddleware::class,
+          when ($app->debugMode, AutoRoutingMiddleware::class),
           'router' => ApplicationRouterInterface::class,
           URLNotFoundMiddleware::class,
         ]);
