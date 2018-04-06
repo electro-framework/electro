72b0a1cd0ba085ac4d1011982d10e7f746854d22
Cláudio Silva <claudio.silva@impactwave.com>
2018-04-06 19:59:37 +0100

    Improved installation resilience.

--- private/src/Bootloader.php
+++ private/src/Bootloader.php
@@ -3,24 +3,33 @@ namespace App;
 
 use Electro\Kernel\Lib\PrimaryBootloader;
 
-require dirname (__DIR__) . '/packages/electro/kernel/src/Kernel/Lib/PrimaryBootloader.php';
+if (@include dirname (__DIR__) . '/packages/electro/kernel/src/Kernel/Lib/PrimaryBootloader.php' !== false) {
 
-/**
- * A class that implements the application's initial bootstrapping sequence, which sets everything else in motion.
- *
- * <p>This subclass just acts as gateway to the framework-provided bootloader. It allows easy access to the bootloader
- * from `index.php` or `workman` scripts and it also allows Composer to find the class when running some event hooks.
- *
- * ><p>**Note:** we are purposefully not loading the class autoloader yet.
- *
- * @see PrimaryBootloader
- */
-class Bootloader extends PrimaryBootloader
-{
-  function __construct ()
+  /**
+   * A class that implements the application's initial bootstrapping sequence, which sets everything else in motion.
+   *
+   * <p>This subclass just acts as gateway to the framework-provided bootloader. It allows easy access to the bootloader
+   * from `index.php` or `workman` scripts and it also allows Composer to find the class when running some event hooks.
+   *
+   * ><p>**Note:** we are purposefully not loading the class autoloader yet.
+   *
+   * @see PrimaryBootloader
+   */
+  class Bootloader extends PrimaryBootloader
   {
-    $this->root = dirname (dirname (__DIR__));
-    parent::__construct ();
+    function __construct ()
+    {
+      $this->root = dirname (dirname (__DIR__));
+      parent::__construct ();
+    }
+
+  }
+
+}
+else {
+
+  class PrimaryBootloader
+  {
   }
 
 }
--- private/src/Installation.php
+++ private/src/Installation.php
@@ -1,7 +1,6 @@
 <?php
 namespace App;
 
-
 class Installation
 {
   /**
